3081010072b57a4b00896dd17ab161dbf588bd28
saharabanu <saharabanungn@gmail.com>
2021-11-30 10:52:41 +0600

    navigation added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -3,19 +3,26 @@ import './Banner.css';
 
 const Banner = () => {
     return (
-      <div className="banner  d-flex  align-items-center justify-content-center ">
+      <div className="banner">
         <div className="row container">
+          
         <div className="col-md-5 col-12">
-          <img style={{width:300 ,height: 200}} src="https://i.ibb.co/NmHvrhG/262402659-1735915976601927-4074890799033005079-n-removebg-preview.png" alt="" />
+          <div className="d-flex  align-items-center justify-content-center mt-2">
+          <img className="rounded-circle" style={{width:200 ,height: 200}} src="https://i.ibb.co/NmHvrhG/262402659-1735915976601927-4074890799033005079-n-removebg-preview.png" alt="" />
+            </div>
           </div>
           <div className="col-md-7 col-12">
-              <h4 className="title">Hello</h4>
-            <h4 className="title">I'm <span style={{color:'rgb(250, 128, 114)'}}>Sahara Banu</span></h4>
-            <h2 className="title-sm"> Junior MERN Stack Developer</h2>
+            <div className="d-flex  align-items-center justify-content-center mt-2">
+            <div>
+            <h2 className="title">Hello! I'm <span style={{color:'rgb(250, 128, 114)'}}>Sahara Banu</span></h2>
+            <h4 className="title-sm"> Junior MERN Stack Developer</h4>
             <br />
-            <a href="https://drive.google.com/file/d/1kk5QGOW7vbRWc-QJXhWOV8NUqJes5Bep/view" target="_blank" rel="noreferrer"style={{textDecoration:'none'}} className="resume">Resume</a>
+            <a href="https://drive.google.com/file/d/1kk5QGOW7vbRWc-QJXhWOV8NUqJes5Bep/view" target="_blank" rel="noreferrer"style={{textDecoration:'none'}} className="resume rounded-pill mb-5"> My Resume</a>
+            </div>
+            </div>
             
           </div>
+         
           
           
         </div>
--- src/Pages/Shared/Navigation/Navigation.js
+++ src/Pages/Shared/Navigation/Navigation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { HashLink } from 'react-router-hash-link';
+import './Navigation.css';
 
 const Navigation = () => {
     return (
@@ -11,11 +12,11 @@ const Navigation = () => {
                     <Navbar.Brand href="#home">Sahara</Navbar.Brand>
                     <Navbar.Toggle />
                     <Navbar.Collapse className="justify-content-end">
-                        <Nav.Link as={HashLink} to="/home#home">Home</Nav.Link>
-                        <Nav.Link as={HashLink} to="/home#about">About</Nav.Link>
-                        <Nav.Link as={HashLink} to="/home#projects">projects</Nav.Link>
-                        <a href="https://drive.google.com/file/d/1kk5QGOW7vbRWc-QJXhWOV8NUqJes5Bep/view" target="_blank" rel="noreferrer"style={{textDecoration:'none'}}>Resume</a>
-                        <Nav.Link as={HashLink} to="/home#contact">Contact</Nav.Link>
+                        <Nav.Link  className="item" as={HashLink} to="/home#home">Home</Nav.Link>
+                        <Nav.Link className="item" as={HashLink} to="/home#about">About</Nav.Link>
+                        <Nav.Link className="item" as={HashLink} to="/home#projects">projects</Nav.Link>
+                        <a href="https://drive.google.com/file/d/1kk5QGOW7vbRWc-QJXhWOV8NUqJes5Bep/view" target="_blank" rel="noreferrer" className="item" style={{textDecoration:'none'}}> My Resume</a>
+                        <Nav.Link className="item" as={HashLink} to="/home#contact">Contact</Nav.Link>
                         
                     </Navbar.Collapse>
                 </Container>
